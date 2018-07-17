48d6484192abf6fc7136435c75da563ea3d5f9ef
anton <nochnoy23>
2018-07-17 20:05:28 +0300

    2-файла

---
+++ src/pkj/one/cu/HelloWorld1.java
@@ -0,0 +1,7 @@
+package pkj.one.cu;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld1");
+    }
+}
---
+++ src/pkj/one/cu/HelloWorld2.java
@@ -0,0 +1,7 @@
+package pkj.one.cu;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld2");
+    }
+}
